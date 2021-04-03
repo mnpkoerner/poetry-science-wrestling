ac1059be802c7412b14f678cc50aa87b9a510423
mnpkoerner <mnpkoerner@gmail.com>
2021-04-02 22:33:33 -0500

    clear placeholders

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+
+import './App.css'
+//test
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          Test text.
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
