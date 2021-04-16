2beeacb4befe7dac1dbecb287e6d346f4d33922f
mnpkoerner <mnpkoerner@gmail.com>
2021-04-16 18:26:12 -0500

    form adds title and body to post object

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,6 +13,7 @@ import Home from '../Home/Home'
 import Register from '../Register/Register'
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 import Test from '../Test/Test'
+import NewPost from '../NewPost/NewPost'
 
 
 function App() {
@@ -24,27 +25,32 @@ function App() {
           <p>
             Test text.
         </p>
-        <Switch >
-          <Route
-            exact
-            path="/">
-            <RPS />
-          </Route>
-          <Route
-            exact
-            path="/home">
-            <Home />
-          </Route>
-          <Route
-            exact
-            path="/register">
-            <Register />
-          </Route>
-          <ProtectedRoute
-            exact
-            path="/test">
+          <Switch >
+            <Route
+              exact
+              path="/">
+              <RPS />
+            </Route>
+            <Route
+              exact
+              path="/home">
+              <Home />
+            </Route>
+            <Route
+              exact
+              path="/register">
+              <Register />
+            </Route>
+            <Route
+              exact
+              path="/newpost">
+                <NewPost/>
+            </Route>
+            <ProtectedRoute
+              exact
+              path="/test">
               <Test />
-          </ProtectedRoute>
+            </ProtectedRoute>
           </Switch>
 
         </header>
--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,7 +1,48 @@
+import {useState} from 'react'
+import {useDispatch} from 'react-redux'
+import {useHistory} from 'react-router-dom'
+
 export default function NewPost() {
+
+    const [post, setPost] = useState({
+        title: '',
+        body: '',
+        date: '',
+        playlist: '',
+
+    })
+
+    const handleClick = (event, keyword) => {
+        event.preventDefault()
+        console.log(keyword)
+        switch(keyword){
+            case "submit":
+                console.log('switch submit')
+                console.log(post)
+        }
+    }
+
+
     return(
         <>
         <h1>Add a new post</h1>
+        <form>
+            <input
+                placeholder="Title"
+                value={post.title}
+                onChange={(event)=>setPost({...post, title: event.target.value})}
+                type="text">
+            </input>
+            <textarea
+                placeholder="body"
+                value={post.body}
+                onChange={(event)=>setPost({...post, body: event.target.value})}>
+            </textarea>
+            <button
+                onClick={(event)=>handleClick(event, 'submit')}>
+                Submit
+            </button>
+        </form>
         </>
 
     )
