3c2fc27a5bce88d4db90b8d1cc860653360c2d2f
mnpkoerner <mnpkoerner@gmail.com>
2021-04-19 22:07:45 -0500

    components added, extra deleted, code cleaned up

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,7 @@
 export default function About() {
     return (
         <div>
-            <p>This is placeholder, but this is where the about page will live
-            
-            </p>
+            <p>This is placeholder, but this is where the about page will live</p>
         </div>
 
     )
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {useHistory} from 'react-router-dom'
 import Login from '../Login/Login'
-import LogOutButton from '../LogOutButton/LogOutButton';
 
 function RegisterForm() {
   const [username, setUsername] = useState('');
