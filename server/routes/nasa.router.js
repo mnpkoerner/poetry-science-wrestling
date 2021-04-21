24fc39bf4263c89e7bfb567123603b819e52d1e6
mnpkoerner <mnpkoerner@gmail.com>
2021-04-20 19:00:51 -0500

    route for NASA API key

---
+++ server/routes/nasa.router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const axios = require('axios');
+
+const router = express.Router();
+
+// return all search images
+// router.get('/:searchterm', (req, res) => {
+//     const giphyList = [];
+
+//     axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${req.params.searchterm}&limit=3`)
+//         .then((response) => {
+//             response.data.data.forEach((gif) => {
+//                 giphyList.push({url: gif.images.original.url, title: gif.title});
+//             });
+//             console.log(`GET Response:`, giphyList);
+//             res.send(giphyList);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//             res.sendStatus(500);
+//         })
+
+// });
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const passport = require('./strategies/user.strategy');
 const userRouter = require('./routes/user.router');
 const demo = require('./routes/demo.router.js');
 const blog = require('./routes/blog.router')
+const nasa = require('./routes/nasa.router')
 
 
 // Body parser middleware
@@ -28,6 +29,7 @@ app.use(passport.session());
 app.use('/api/user', userRouter);
 app.use('/api/demo', demo);
 app.use('/api/blog', blog);
+app.use('/api/nasa', nasa)
 
 
 // Serve static files
