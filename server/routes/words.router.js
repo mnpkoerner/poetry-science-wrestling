91e5855dce8f45f23719c22b5757ea3c68d43a93
mnpkoerner <mnpkoerner@gmail.com>
2021-04-22 18:57:07 -0500

    server side words routers edited to get synonyms and rhyming words

--- server/routes/words.router.js
+++ server/routes/words.router.js
@@ -4,15 +4,34 @@ const axios = require('axios').default;
 
 const router = express.Router();
 
-router.get('/:word', (req, res) => {
+//get request to get synonym
+router.get('/:synonym', (req, res)=>{
+    console.log(`in synonym with ${req.params.synonym}`)
     axios.request({
         method: 'GET',
-        url: `https://wordsapiv1.p.rapidapi.com/words/${req.params.word}/pertainsTo`,
+        url: `https://wordsapiv1.p.rapidapi.com/words/${req.params.synonym}/synonyms`,
         headers: {
-            'x-rapidapi-key': `${process.env.WORDS}`,
-            'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com'
+        'x-rapidapi-key': `${process.env.WORDS}`,
+        'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com'
         }
-    }).then(function (response) {
+  }).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+})
+
+//get request to get rhyming word
+router.get('/:rhyme', (req, res)=>{
+    console.log(`in synonym with ${req.params.synonym}`)
+    axios.request({
+        method: 'GET',
+        url: `https://wordsapiv1.p.rapidapi.com/words/${req.params.rhyme}/rhymes`,
+        headers: {
+        'x-rapidapi-key': `${process.env.WORDS}`,
+        'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com'
+        }
+  }).then(function (response) {
         console.log(response.data);
     }).catch(function (error) {
         console.error(error);
