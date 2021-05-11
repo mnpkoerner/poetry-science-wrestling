20ea4c817d645d4cf00520e705e43ec4aaa17b3c
mnpkoerner <mnpkoerner@gmail.com>
2021-05-11 15:27:46 -0500

    update KFAI page

--- src/components/KFAI/KFAI.js
+++ src/components/KFAI/KFAI.js
@@ -1,5 +1,34 @@
-export default function KFAI () {
-    return(
-        <p>This is where links to KFAI will live. Donate, archive, etc.</p>
+export default function KFAI() {
+    return (
+        <div className="text-container">
+            <header>
+                <h1>KFAI Community Radio:</h1>
+            </header>
+            <main>
+                <h3>Mission:</h3>
+                <p>
+                    KFAI is a volunteer-based community radio station that exists to broadcast information, arts and entertainment programming
+                    for an audience of diverse racial, social and economic backgrounds. By providing a voice for people ignored or misrepresented
+                    by mainstream media, KFAI increases understanding between peoples and communities, while fostering the values of
+                    democracy and social justice.
+                </p>
+                <h3>About:</h3>
+                <p>
+                    KFAI is a non-commercial FM radio station operating on 90.3 MHz in Minneapolis and covering the greater Twin Cities.
+                    KFAI is governed by a volunteer community Board of Directors, managed by a small paid full-time and part-time staff,
+                    and operated by a volunteer base of over 300. KFAI’s budget for 2021 is approximately $670,000.
+
+                        <br></br>
+
+                    KFAI is listener-supported, with over 3,000 members contributing nearly 50 percent of its income.
+                    Additional support has come from the Corporation for Public Broadcasting, the State of Minnesota, Legacy and Cultural Heritage Funding
+                    from MN State, the Minneapolis Foundation, and program underwriters. KFAI has been broadcasting since 1978 with
+                    major signal expansions in 1982, 1994 and in 2008. It is currently broadcasting at 900 watts (directional)
+                    from the top of the IDS Center in downtown Minneapolis.
+                </p>
+                <a href="http://www.kfai.org">Homepage</a>
+                <a href="http://www.kfai.org/donate">Donate</a>
+            </main>
+        </div>
     )
 }
